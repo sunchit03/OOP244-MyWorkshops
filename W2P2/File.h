b687ba3a06109f7c14e7dd5eb7bf13ccdb3c317c
Sunchit Singh <sunchit-singh@myseneca.ca>
2022-09-24 03:58:27 -0400

    Utils.cpp, with custom funcs added

--- W2P2/File.h
+++ W2P2/File.h
@@ -15,7 +15,7 @@
 ***********************************************************************/
 #ifndef SDDS_FILE_H_
 #define SDDS_FILE_H_
-//#include "Population.h"
+
 namespace sdds {
    // Opens a file and keeps the file pointer in the File.cpp (file scope)
    bool openFile(const char filename[]);
@@ -24,8 +24,6 @@ namespace sdds {
    // Finds the records kept in the file to be used for the dynamic memory allocation 
    // of the records in the file
    int noOfRecords();
-
-   //bool readPCP(PCpopulation& PCp, const char filename[]);
 
    extern FILE* fptr;
 }
