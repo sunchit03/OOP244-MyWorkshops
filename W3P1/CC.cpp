35d75ee04dddb2112b637d3c3906b15f29a7c33a
Sunchit Singh <sunchit-singh@myseneca.ca>
2022-09-26 05:41:00 -0400

    display func left only

--- W3P1/CC.cpp
+++ W3P1/CC.cpp
@@ -1,41 +1,154 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
 #include "CC.h"
+#include "Utils.h"
+
+using namespace std;
 
 namespace sdds {
+
+   CC info{};
+
    bool CC::validate(const char* name, unsigned long long cardNo, short cvv, short expMon, short expYear) const {
-   
-      return false;
+      bool retVal{};
+
+      if (name != nullptr && strlen(name) > 2) {
+         if (cardNo >= 4000000000000000 && cardNo <= 4099999999999999) {
+            if (cvv >= 100 && cvv <= 999) {
+               if (expMon >= 1 && expMon <= 12) {
+                  if (expYear >= 22 && expYear <= 32) {
+                     retVal = true;
+                  }
+                  else {
+                     retVal = false;
+                  }
+               }
+               else {
+                  retVal = false;
+               }
+            }
+            else {
+               retVal = false;
+            }
+         }
+         else {
+            retVal = false;
+         }
+      }
+      else {
+         retVal = false;
+      }
+
+      return retVal;
    
    }
 
    void CC::prnNumber() const {
-   
+      int first, second, third, fourth;
+      first = info.m_cardNo / 1000000000000;
+      second = (info.m_cardNo % 1000000000000) / 100000000;
+      third = (info.m_cardNo % 100000000) / 10000;
+      fourth = info.m_cardNo % 10000;
+
+      cout << first << " " << second << " " << third << " " << fourth;
    }
 
    void CC::set() {
-   
+      info.m_name = nullptr;
+      info.m_cardNo = 0;
+      info.m_CVV = 0;
+      info.m_expMon = 0;
+      info.m_expYear = 0;
    }
 
    void CC::cleanUp() {
-   
+      delete[] info.m_name;
+      info.set();
    }
    
    bool CC::isEmpty() const {
-   
-      return false;
-   
+      return (info.m_name == nullptr);
    }
 
    void CC::set(const char* cc_name, unsigned long long cc_no, short m_cvv, short m_expMon, short m_expYear) {
-   
+      cleanUp();
+      if (info.validate(cc_name, cc_no, m_cvv, m_expMon, m_expYear)) {
+         info.m_name = new char[strlen(cc_name) + 1];
+         strcpy(info.m_name, cc_name);
+
+         info.m_cardNo = cc_no;
+         info.m_CVV = m_cvv;
+         info.m_expMon = m_expMon;
+         info.m_expYear = m_expYear;
+      }
    }
 
    bool CC::read() {
-   
-      return false;
-   
+      char tempName[NAME_CHARS + 1] = "";
+      short tempCVV;
+      short tempExpMon;
+      short tempExpYear;
+      unsigned long long tempCardNo;
+
+      bool retVal = false;
+
+      info.cleanUp();
+
+      cout << "Card holder name: ";
+      cin.getline(tempName, 71);
+      if (!cin.fail()) {
+         cout << "Credit card number: ";
+      }
+      cin >> tempCardNo;
+
+      if (!cin.fail()) {
+         cout << "Card Verification Value (CVV): ";
+      }
+      cin >> tempCVV;
+
+      if (!cin.fail()) {
+         cout << "Expiry month and year (MM/YY): ";
+      }
+      cin >> tempExpMon;
+      cin.ignore(1, ',');
+      cin >> tempExpYear;
+
+      if (!cin.fail()) {
+         info.set(tempName, tempCardNo, tempCVV, tempExpMon, tempExpYear);
+
+         cin.ignore(1000, '\n');
+      }
+      else {
+         cin.clear();
+         cin.ignore(1000, '\n');
+      }  
+      
+      return !info.isEmpty();
    }
 
+
    void CC::display(int row) const {
-   
+
+      if (info.isEmpty()) {
+         cout << "Invalid Credit Card Record" << endl;
+      }
+      else {
+         if (row >= 1) {
+            cout << "| " << setw(3) << row << " | " << left << setw(30) << info.m_name << " | ";
+            info.prnNumber();
+            cout << " | " << info.m_CVV << " | " << setw(2) << info.m_expMon << "/" << info.m_expYear << endl;
+         }
+         else if (row <= 0)
+         {
+            cout << "Name: " << info.m_name << endl;
+            cout << "Creditcard No: ";
+            info.prnNumber();
+            cout << endl;
+            cout << "Card Verification Value: " << info.m_CVV << endl;
+            cout << "Expiry Date: " << info.m_expMon << "/" << info.m_expYear << endl;
+         }
+      }
    }
 }
--- W3P1/CC.h
+++ W3P1/CC.h
@@ -3,12 +3,14 @@
 
 namespace sdds {
 
+   const int NAME_CHARS = 71;
+
    class CC {
       char* m_name;
       short m_CVV;
       short m_expMon;
       short m_expYear;
-      unsigned long long cardNo;
+      unsigned long long m_cardNo;
 
    private:
       bool validate(const char* name,
@@ -20,12 +22,24 @@ namespace sdds {
       void prnNumber()const;
 
    public:
+      // Sets the object to a safe empty state by setting
+      // all the values to zero and nullptr.
       void set();
 
+      // First, it will deallocate the cardholder's name 
+      // and then calls the set() to set the object to a safe empty state.
       void cleanUp();
 
+      // Returns if the CC object is in a safe empty state or not; 
+      // by returning true if the cardholder name pointer attribute
+      // of the object is nullptr, otherwise, it will return false.
       bool isEmpty() const;
 
+      //First, it will cleanUp() the object.Then if all the arguments
+      // are valid using validate(), it will dynamically keep a copy 
+      // of the name in the name attributeand set the rest of the 
+      // attributes to their corresponding values.
+      // If any of the arguments are invalid, nothing will be set.
       void set(const char* cc_name,
          unsigned long long cc_no,
          short m_cvv,
