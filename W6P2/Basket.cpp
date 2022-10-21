b68d15b0ea51264db543bf8bf33d4f963f583e25
Sunchit Singh <sunchit-singh@myseneca.ca>
2022-10-21 15:07:24 -0400

    functions declared

--- W6P2/Basket.cpp
+++ W6P2/Basket.cpp
@@ -1,5 +1,59 @@
+/*
+Full Name: Sunchit Singh
+Email    : sunchit-singh@myseneca.ca
+StudentID: 169146214
+Date     : October 17, 2022
+*/
+
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
 #include "Basket.h"
 
 namespace sdds {
+   Basket::Basket() {
+      m_fruits = nullptr;
+      m_size = 0;
+      m_price = 0;
+   }
 
+   Basket::Basket(Fruit fruitArr[], int size, double price) {
+      if (fruitArr && size > 0 && price > 0) {
+         m_size = size;
+         m_price = price;
+         m_fruits = new Fruit[m_size];
+         
+         for (int i = 0; i < size; i++) {
+            strcpy(m_fruits[i].m_name, fruitArr[i].m_name);
+            m_fruits[i].m_qty = fruitArr[i].m_qty;
+         }
+      }
+   }
+
+   Basket::Basket(const Basket& toCopyFrom) {
+
+   }
+
+   Basket& Basket::operator=(const Basket& toCopyFrom) {
+      // TODO: insert return statement here
+   }
+
+   Basket::~Basket() {
+
+   }
+
+   void Basket::setPrice(double price) {
+
+   }
+
+   Basket::operator bool() {
+
+   }
+
+   Basket& Basket::operator+=(const Fruit rightOperand) {
+      // TODO: insert return statement here
+   }
+
+   istream& operator<<(istream& istr, const Basket& rightOperand) {
+      // TODO: insert return statement here
+   }
 }
--- W6P2/Basket.h
+++ W6P2/Basket.h
@@ -1,8 +1,40 @@
+/*
+Full Name: Sunchit Singh
+Email    : sunchit-singh@myseneca.ca
+StudentID: 169146214
+Date     : October 17, 2022
+*/
+
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
 #ifndef SDDS_BASKET_H_
 #define SDDS_BASKET_H_
-
+#include <iostream>
+using namespace std;
 namespace sdds {
 
+   struct Fruit {
+      char m_name[30 + 1]; // the name of the fruit
+      double m_qty;        // quantity in kilograms
+   };
+
+   class Basket {
+   private:
+      Fruit* m_fruits{};
+      int m_size;
+      double m_price;
+
+   public:
+      Basket();
+      Basket(Fruit fruitArr[], int size, double price);
+      Basket(const Basket& toCopyFrom);
+      Basket& operator=(const Basket& toCopyFrom);
+      ~Basket();
+      void setPrice(double price);
+      operator bool();
+      Basket& operator+=(const Fruit rightOperand);
+   };
+   istream& operator<<(istream& istr, const Basket& rightOperand);
 }
 
 #endif
